4e49c97409da7fe52d38ff3e3e8a2dd45f611404
Pratik Singh <pratiksjv@gmail.com>
2018-09-09 19:42:34 +0530

    typo corrected

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export default {
   api: {
-    baseurl: 'http://api.openweathermap.org/data/2.5/',
+    baseurl: 'https://api.openweathermap.org/data/2.5/',
     imageurl: 'https://openweathermap.org/img/w/',
     appid: '0fc47171c38320af914c66f014c03d1d',
   },
